73b38676b0923796a6e2dac207a8fec8a3c5ffbe
Rahul <rahulbiswalnew@gmail.com>
2023-06-12 01:02:45 +0530

    component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainMenuComponent } from './header/main-menu/main-menu.component';
+import { TopMenuComponent } from './header/top-menu/top-menu.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainMenuComponent,
+    TopMenuComponent,
+    ProductListComponent,
+    TopHeaderComponent
   ],
   imports: [
     BrowserModule
